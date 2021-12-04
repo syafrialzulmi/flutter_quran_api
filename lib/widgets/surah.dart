b98b61c0cce846ca23d228641b2e0657a907a4ef
Syafrial Zulmi <syafrialzulmi@gmail.com>
2021-12-04 21:17:33 +0700

    update listTile

--- lib/widgets/surah.dart
+++ lib/widgets/surah.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/svg.dart';
+import '../widgets/surah_item.dart';
 
 class Surah extends StatelessWidget {
   const Surah({
@@ -8,77 +8,8 @@ class Surah extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-        child: ListTile(
-      leading: Container(
-        height: 50,
-        width: 50,
-        child: Stack(
-          alignment: Alignment.center,
-          children: [
-            Container(
-                height: 50,
-                width: 50,
-                child: SvgPicture.asset('assets/icons/banner_ayat.svg')),
-            Text(
-              '1',
-              style: TextStyle(
-                  fontFamily: 'Poppins',
-                  fontSize: 14,
-                  fontWeight: FontWeight.bold,
-                  color: Color(0xffA44AFF)),
-            )
-          ],
-        ),
-      ),
-      title: Text(
-        'data 2',
-        style: TextStyle(
-            fontFamily: 'Poppins',
-            fontSize: 16,
-            fontWeight: FontWeight.bold,
-            color: Colors.white),
-      ),
-      subtitle: Row(
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: <Widget>[
-          Text(
-            'data 3.1'.toUpperCase(),
-            style: TextStyle(
-                fontFamily: 'Poppins',
-                fontSize: 12,
-                fontWeight: FontWeight.bold,
-                color: Color(0xff7B80AD)),
-          ),
-          SizedBox(
-            width: 5,
-          ),
-          Icon(
-            Icons.circle,
-            color: Color(0xff7B80AD),
-            size: 8,
-          ),
-          SizedBox(
-            width: 5,
-          ),
-          Text(
-            'data 3.2'.toUpperCase(),
-            style: TextStyle(
-                fontFamily: 'Poppins',
-                fontSize: 12,
-                fontWeight: FontWeight.bold,
-                color: Color(0xff7B80AD)),
-          ),
-        ],
-      ),
-      trailing: Text(
-        'data 4',
-        style: TextStyle(
-          fontFamily: 'Amiri',
-          fontSize: 20,
-          color: Color(0xffA44AFF),
-        ),
-      ),
-    ));
+    return ListView(children: <Widget>[
+      SurahItem(),
+    ]);
   }
 }
